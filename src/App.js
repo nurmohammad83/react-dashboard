0b1ddef9e72e5d5ad8721053f582f9e566d7e4d9
Nur Mohammad <mohammadnur5480@gmail.com>
2023-07-02 10:33:04 +0600

    Added chart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
-import {TooltipComponent} from '@syncfusion/ej2-react-popups';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
-
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const activeMenu =true;
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
   return (
-    <div>
-    <BrowserRouter>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+      <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <TooltipComponent
@@ -21,8 +32,8 @@ const App = () => {
             >
               <button
                 type="button"
-               
-                style={{ background: 'blue', borderRadius: '50%' }}
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
@@ -50,7 +61,7 @@ const App = () => {
               <Navbar />
             </div>
             <div>
-             
+              {themeSettings && (<ThemeSettings />)}
 
               <Routes>
                 {/* dashboard  */}
